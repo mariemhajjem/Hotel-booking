ebde69594d85288c420c7e2bba43ee81bb7c9e9b
mariemhajjem <mariemhajjem10@gmail.com>
2025-07-20 22:56:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'> 
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
