986c58bbeecf7ee74512356aef7581490523f05e
mariemhajjem <mariemhajjem10@gmail.com>
2025-07-22 20:41:56 +0100

    create hotel room details

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RooomDetails from './pages/RooomDetails'
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner")
@@ -13,6 +15,8 @@ const App = () => {
       <div className='min-h-[70vh]'> 
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/rooms' element={<AllRooms />} />
+            <Route path='/rooms/:id' element={<RooomDetails />} />
         </Routes>
       </div>
       <Footer />
